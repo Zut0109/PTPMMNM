910fe78328019b553f551e6c49433452295c9528
Zut0109 <hoainguyen.010901@gmail.com>
2022-10-07 23:26:46 +0700

    react update

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::prefix('posts')->name('posts.')->group(function () {
-    Route::get('', 'PostController@index')->name('index'); //Danh sách;
-    Route::post('{post}', 'PostController@store')->name('store'); //Lưu;
-    Route::get('{post}', 'PostController@show')->name('show'); //Chi tiết;
-    Route::post('{post}', 'PostController@update')->name('update'); //Cập nhập;
-    Route::post('delete/{post}', 'PostController@delete')->name('delete'); //Xóa;
-});
