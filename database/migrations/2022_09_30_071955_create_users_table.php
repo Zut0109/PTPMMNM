993ae6765aaaf562f864ff03ab587b2fdcd9fd81
Zut0109 <hoainguyen.010901@gmail.com>
2022-10-07 14:05:25 +0700

    user

--- database/migrations/2022_09_30_071955_create_users_table.php
+++ database/migrations/2022_09_30_071955_create_users_table.php
@@ -14,11 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->increments('UserId')->unique();
+            $table->string('UserName');
+            $table->string('UserMail');
+            $table->string('UserPass');
+            $table->string('UserPhone');
+            $table->string('UserAddress');
             $table->rememberToken();
             $table->timestamps();
         });
